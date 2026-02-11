1b5746bb65111c08b6c4c1f9e46f20e1befc06ef
caleb <calebbraziel1@gmail.com>
2026-02-11 10:23:17 -0500

    Initialized variables that will be used throughout the script

--- button.gd
+++ button.gd
@@ -1,5 +1,25 @@
 extends Button
+@onready var grid := $"../../../Inventory/MarginContainer/GridContainer"
 
+var item_slots: Array[Panel]
+
+var letter_bag: Array[String] = []
+
+var letters: Array[String] = [
+	".png",
+	"A.png", "B.png", "C.png", "D.png", "E.png", "F.png", "G.png", "H.png", "I.png",
+	"J.png", "K.png", "L.png", "M.png", "N.png", "O.png", "P.png", "Q.png", "R.png",
+	"S.png", "T.png", "U.png", "V.png", "W.png", "X.png", "Y.png", "Z.png"
+]
+
+var letter_count := [
+	2,
+	9, 2, 2, 4, 12, 2, 3, 2, 9,
+	1, 1, 4, 2, 6, 8, 2, 1, 6,
+	4, 6, 4, 2, 2, 1, 2, 1
+]
+
+var random_num := RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
